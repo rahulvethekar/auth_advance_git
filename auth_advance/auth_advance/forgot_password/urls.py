bbc0511de5dc5fcf975a102605c90f64ec250b29
rahulvethekar <76589326+rahulvethekar@users.noreply.github.com>
2021-11-14 11:49:13 +0530

    Delete auth_advance/auth_advance/forgot_password directory

--- auth_advance/auth_advance/forgot_password/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import forgotPassword,forgotDetail
-
-urlpatterns = [
-    path('forgotPassword/',forgotPassword,name = 'forgotPassword'),
-    path('resetPassword/',forgotDetail,name = 'reset'),
-
-]
